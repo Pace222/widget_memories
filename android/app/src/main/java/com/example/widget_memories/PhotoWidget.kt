c5e236be07ace04a6d25862bcd71e6c9ca0c9423
Pierugo Pace <pierugo.pace@gmail.com>
2024-12-08 14:20:22 +0100

    Android part when no image is available

--- android/app/src/main/java/com/example/widget_memories/PhotoWidget.kt
+++ android/app/src/main/java/com/example/widget_memories/PhotoWidget.kt
@@ -5,6 +5,8 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.widget.ImageView
 import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetPlugin
 import java.io.File
@@ -23,18 +25,32 @@ class PhotoWidget : AppWidgetProvider() {
             val widgetData = HomeWidgetPlugin.getData(context)
             val views = RemoteViews(context.packageName, R.layout.photo_widget).apply {
                 val imageName = widgetData.getString("filename", null)
-                val imageFile = File(imageName)
-                val imageExists = imageFile.exists()
-                if (imageExists) {
-                    val myBitmap: Bitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
-                    setImageViewBitmap(R.id.widget_image, myBitmap)
-                } else {
-                    println("image not found!, looked @: ${imageName}")
+                
+                val bitmap: Bitmap = when {
+                    imageName == null -> createBlackImage()
+                    else -> {
+                        val imageFile = File(imageName)
+                        if (imageFile.exists()) {
+                            BitmapFactory.decodeFile(imageFile.absolutePath)
+                        } else {
+                            createBlackImage()
+                        }
+                    }
                 }
+
+                setImageViewBitmap(R.id.widget_image, bitmap)
             }
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
+    }
+
+    fun createBlackImage(id: Int = R.id.widget_image): Bitmap {
+        val width = 200
+        val height = 200
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        bitmap.eraseColor(Color.BLACK) // Fill the bitmap with black color
+        return bitmap
     }
 }
 
